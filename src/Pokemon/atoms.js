3c1ed68627afc3f07048faec970ebeafd9bf7587
ajaythapliyal <ajaythapliyal1703@gmail.com>
2024-05-18 17:55:13 +0530

    Restructures atoms to allow client side search

--- src/Pokemon/atoms.js
+++ src/Pokemon/atoms.js
@@ -4,21 +4,33 @@ import { delay } from '../util';
 
 const LIMIT = 50;
 
+export const pokemonIdSetAtom = atom(async ()=>{
+    const pokemonIds= await fetchPokemonList(10000, 0);
+    const pokemonIdSet = new Set(pokemonIds.map(pokemonId => pokemonId.name.trim()))
+    return pokemonIdSet;
+})
 
-
-const _pokemonAtom = atom([]);
+const _pokemonAtom = atom(new Map());
 
 export const isPokemonFetching = atom(false)
 
 export const pokemonAtom = atom(
-    (get)=> get(_pokemonAtom), 
+    (get)=> Array.from(get(_pokemonAtom).values()), 
     async (get, set, newVal) => {
         set(isPokemonFetching, true)
-         await delay(1000)
+        await delay(1500)
         const cachedPokemons = get(_pokemonAtom)
-        const pokemonList = await fetchPokemonList(LIMIT, cachedPokemons.length);
-        const pokemons = await fetchPokemons(pokemonList.map(pokemon => pokemon.name))
-        set(_pokemonAtom,[...cachedPokemons, ...pokemons] )
+        const pokemonIdSet = await get(pokemonIdSetAtom)
+        const cachedPokemonIds = cachedPokemons.keys()
+        const pokemonFetchId = Array.from(pokemonIdSet.difference(new Set(cachedPokemonIds))).slice(0, LIMIT)
+        const pokemons = await fetchPokemons(pokemonFetchId)
+        set(_pokemonAtom,new Map([...cachedPokemons, ...mapPokemon(pokemons)]))
         set(isPokemonFetching, false)
     }
 )
+
+const mapPokemon = (pokemons)=>{
+    const pokemonMap =  new Map();
+    pokemons.forEach(pokemon => pokemonMap.set(pokemon.name, pokemon))
+    return pokemonMap
+}
